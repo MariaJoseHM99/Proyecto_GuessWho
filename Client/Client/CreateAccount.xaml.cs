79d04711d81d69a3360a8cdd2ac550d1079b9d92
MARIA JOSE HERNANDEZ MOLINOS <zS17012956@estudiantes.uv.mx>
2019-10-11 12:15:59 -0500

    Prueba Inserción 2

--- Client/Client/CreateAccount.xaml.cs
+++ Client/Client/CreateAccount.xaml.cs
@@ -11,9 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-//Holaaaaaa
-//    como
-//    estan
+//Probando 2
 namespace Client {
     /// <summary>
     /// Lógica de interacción para CreateAccount.xaml
